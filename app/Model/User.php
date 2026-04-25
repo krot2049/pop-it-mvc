dd43b9af45eeb3e92173195e7b64e1d215b308c5
yogurt <ackermanqpp0912@mail.ru>
2026-04-26 00:32:03 +0700

    модель user и таблица пользователей

---
+++ app/Model/User.php
@@ -0,0 +1,46 @@
+<?php
+namespace Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Src\Auth\IdentityInterface;
+
+class User extends Model implements IdentityInterface
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'login',
+        'password',
+        'role'
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($user) {
+            $user->password = md5($user->password);
+        });
+    }
+
+    public function findIdentity(int $id)
+    {
+        return self::where('id', $id)->first();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function attemptIdentity(array $credentials)
+    {
+        return self::where([
+            'login' => $credentials['login'],
+            'password' => md5($credentials['password'])
+        ])->first();
+    }
+}
